a57312182500da4a9b7d560ca009363c1edfdd4f
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2022-11-23 13:29:01 -0800

    land fix

--- src/Sections/Landing.jsx
+++ src/Sections/Landing.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function Landing() {
     return(
         <div className='Landing-Page'>
-            <div className='Landing-Text'>
-                <h1>Bridger Brown</h1>
-                <h2>Front-End Web Developer</h2>
+            <div className='Landing-box'>
+                <div className='Landing-Text'>
+                    <h1>Bridger Brown</h1>
+                    <h2>Front-End Web Developer + Designer</h2>
+                </div>
+                <img src='./src/assets/mars-crop.png' alt='mars' id="mars-circle"/>
             </div>
         </div>
     )
