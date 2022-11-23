03e79cbba3777ffe35b5d1b0224fe6a60579d30b
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2022-11-23 14:37:17 -0800

    css finetuning

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -12,7 +12,6 @@ function Contact() {
         <div className="contact-container" id="contact">
             <h3 className='Section-Headers'>Contact</h3>
             <div className="contact-items">
-                <h2>Get in Touch</h2>
                 <p>I am currently available for work. Feel free to reach out to me for any inquiries!</p>
                 <button onClick={copyContact} id="copy-button">Copy Email</button>
             </div>
