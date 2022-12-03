9c3a88ea2ff746c17b0fc18365c6e7bd9f4be574
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2022-12-03 15:16:08 -0800

    Procfile

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import Home from './sections/Home.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
