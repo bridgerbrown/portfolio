061edab099bcf154daa8f901ca35cf59ad92637a
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2022-12-05 12:35:52 -0800

    revert vite config

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: 'portfolio-website/index.html',
-      name: pkg.name
-    },
-    rollupOptions: {
-      input: {
-        'entry-point-a': 'portfolio-website/index.html',
-      }
-    }
-  }
+  plugins: [react()]
 })
