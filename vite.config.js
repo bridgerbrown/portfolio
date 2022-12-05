8c50bdf37f4ce46e56e75c498247c65bff73f658
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2022-12-05 12:26:41 -0800

    vite config try

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    commonjsOptions: {
-      include: [/node_modules/]
+    rollupOptions: {
+      input: {
+        'entry-point-a': path.resolve(__dirname, 'src/main.jsx'),
+      }
     }
   }
 })
