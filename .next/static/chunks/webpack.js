7c1c231dfb6555c166fc5da397deac97fc7beb55
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2022-12-10 15:44:35 -0800

    fixed sidebar css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0e7eba3650f16c2"; }
+/******/ 		__webpack_require__.h = function() { return "72d03f525d5776eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
